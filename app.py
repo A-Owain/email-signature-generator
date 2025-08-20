ded1a28046c513149f87fa9a3619c553f1aaa5d6
Abdulrrahman M. Alowain <abdulrrahman.alowain@Marketing-Support.local>
2025-08-20 15:17:33 +0300

    Add Procfile and fix app port for Railway

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = position = email = phone = ''
+    signature = None
     if request.method == 'POST':
         name = request.form.get('name')
         position = request.form.get('position')
         email = request.form.get('email')
         phone = request.form.get('phone')
-
-    return render_template('index.html', name=name, position=position, email=email, phone=phone)
+        signature = {
+            'name': name,
+            'position': position,
+            'email': email,
+            'phone': phone
+        }
+    return render_template('index.html', signature=signature)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use PORT env var if available (for deployment), else default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
