cc23f24a6d1cfce2f65bf74adf1bebdd53e985e7
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-09-22 23:04:21 +0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,9 +1,19 @@
 // Define department roles
 const rolesByDepartment = {
-    Marketing: ["Marketing Specialist", "Brand Manager", "Content Lead"],
-    Finance: ["Financial Analyst", "Controller", "Accountant"],
-    IT: ["Backend Developer", "Frontend Developer", "IT Support"],
-    HR: ["HR Manager", "Recruiter", "People Partner"]
+    PMO: ["Head of Strategy", "Head of PMO", "IT Program Manager", "Business Analyst"],
+    IT: ["Head of IT", "Senior Manager of Technology Operation & Applications", "Senior Analyst", "Senior Technical & Network Engineer", "IT Support Administrator", "Senior DevOps Engineer", "Outsourced"],
+    Engineering: ["Head of Engineering", "Senior Software Engineer", "Staff IOS Engineer", "Senior Analyst", "Outsourced"],
+    Data: ["Head of Data",  "Analyst", "Associate Data Engineer", "Data Engineer", "Data Analyst", "Outsourced"],
+    Product: ["Principal", "Lead", "Senior"],
+    Cybersecurity: ["CISO", "Cybersecurity Engineer", "Cybersecurity Analyst", "Threat Intelligence Analyst", "Outsourced"],
+    Risk: ["Head of Risk", "Risk Officer", "AVP", "Credit Manager", "Credit underwriting senior analyst", "Credit Underwriting Analyst", "Credit Underwriting Analyst", "Credit Underwriting Analyst", "AVP - Operations Manager", "Senior Associate", "Associate", "Senior Operations Analyst", "Senior Analyst", "Analyst", "Credit Administration Analyst", "Operations Analyst", "Operations Analyst"],
+    Legal: ["Senior Associate", "Associate", "Senior legal analyst", "Team Leader", "Legal Analyst", "Analyst", "Collection Manager", "Senior Analyst", "Senior Collection Officer", "Collection Officer", "Senior Corporate government Analyst"],
+    Customer_Care: ["Head of Customer Care", "Senior Customer Care Specialist", "Customer Care Specialist"],
+    Compliance: ["VP - Head of Compliance", "Senior AML Officer", "AML & Compliance Officer", "Compliance Specialist"],
+    Digital_Lending: ["Commercial Director", "Digital Marketing Manager", "Customer Success Manager", "Customer Success Specialist"],
+    Direct_Lending: ["Head of Sales", "Sales Manager", "Senior Team Leader", "Team Leader", "Growth Manager", "Growth Consultant II", "Growth Consultant I", "Growth Associate", "Key Accounts Manager", "Senior Key Accounts Manager", "Analyst", "Sales Operations", "Business Development Representative", "Quality Assurance Specialist"],
+    Marketing: ["Senior Communication Specialist", "Marketing Executive", "Social Media Specialist", "Marketing Specialist"],
+    Finance: ["VP - Head of Finance", "VP - Finance", "Senior Financial Associate", "Financial Associate", "Senior Finance Analyst", "Financial Analyst"]
 };
 
 // Populate role dropdown when department changes
